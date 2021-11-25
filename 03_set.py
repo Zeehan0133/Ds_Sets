e9e5adf54bc0dd64b498d630ce26474a9f794401
Zeehan0133 <zyne0133@gmail.com>
2021-11-26 01:18:55 +0530

    [zeehan] add:(add members in set )

---
+++ 03_set.py
@@ -0,0 +1,17 @@
+"""
+@Author: Zeehan 
+@Date: 2021-25-11 19:00:30
+@Title : add member in a set
+"""
+"""
+Description:
+            add member with the help of add method
+     
+Parameter:
+      
+Return:
+"""
+member={"rahul","ravi","Rehan"}
+print(member)
+member.add("Zeehan")
+print(member)
