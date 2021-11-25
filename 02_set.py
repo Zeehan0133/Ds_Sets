dc8b8be52ad5685b2ccaacf104dd6540993226fd
Zeehan0133 <zyne0133@gmail.com>
2021-11-26 01:17:40 +0530

    [zeehan] add:(iterate over set )

---
+++ 02_set.py
@@ -0,0 +1,16 @@
+"""
+@Author: Zeehan 
+@Date: 2021-25-11 19:00:30
+@Title : iteration over set
+"""
+"""
+Description:
+            declare a set and itrate for loop and print the result 
+     
+Parameter:
+      
+Return:
+"""
+setx={1,2,3,4,5,6}
+for i in (setx):
+    print(i)
