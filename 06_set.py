e07f8f3cd23837d78cf89a414a0434b2c8bb083d
Zeehan0133 <zyne0133@gmail.com>
2021-11-26 01:23:56 +0530

    [zeehan] add:(intersection in set  )

---
+++ 06_set.py
@@ -0,0 +1,18 @@
+"""
+@Author: Zeehan 
+@Date: 2021-25-11 19:00:30
+@Title : intersection in set 
+"""
+"""
+Description:
+     declare variable and intersect it with the help of.intersection
+
+Parameter:
+      
+Return:
+"""
+a={1,2,3,4,5}
+b={5,6,7,8,9}
+print("before intersection :" ,(a) ,(b))
+i=a.intersection(b)
+print("after intersection :" ,(i))
