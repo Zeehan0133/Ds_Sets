79304b174d2f72918967ad0ac5b617cafe7b2e51
Zeehan0133 <zyne0133@gmail.com>
2021-11-26 01:15:18 +0530

    [zeehan] add:(create a set)

---
+++ 01_set.py
@@ -0,0 +1,15 @@
+"""
+@Author: Zeehan 
+@Date: 2021-25-11 19:00:30
+@Title : create a set
+"""
+"""
+Description:
+            create a set and print the element inside the set
+     
+Parameter:
+      
+Return:
+"""
+setx={1,2,3,4,5,6}
+print(setx)
