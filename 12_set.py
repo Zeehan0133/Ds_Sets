ed61bd5ed78321ba80a6d85e2e595dd15c5a368d
Zeehan0133 <zyne0133@gmail.com>
2021-11-26 01:40:00 +0530

    [zeehan] add:(find max and minimum in set )

---
+++ 12_set.py
@@ -0,0 +1,20 @@
+"""
+@Author: Zeehan 
+@Date: 2021-25-11 19:00:30
+@Title : find max and minimum value of the set
+"""
+"""
+Description:
+          firstly we declare one set and we achived max and minimum value with the help of min and max function 
+
+Parameter:
+      
+Return:
+"""
+setn = {5, 10, 3, 15, 2, 20}
+print("Original set elements:")
+print(setn)
+print("Maximum value of the said set:")
+print(max(setn))
+print("Minimum value of the said set:")
+print(min(setn))
