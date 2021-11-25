5a7c77b56cf02541c10339d3e48c378aaa8fdca9
Zeehan0133 <zyne0133@gmail.com>
2021-11-26 01:30:46 +0530

    [zeehan] add:(symmetric difference of 2 sets  )

---
+++ 09_set.py
@@ -0,0 +1,17 @@
+"""
+@Author: Zeehan 
+@Date: 2021-25-11 19:00:30
+@Title : symmetric difference of 2 sets 
+"""
+"""
+Description:
+     symmetric difference of two sets is nothing but the union of (a-b) and (b-a) 
+
+Parameter:
+      
+Return:
+"""
+seta={1,2,3,4,5,6,7,8}
+setb={1,3,5,6,7,8,9}
+symd=seta ^ setb
+print("symmetric difference is " , (symd))
