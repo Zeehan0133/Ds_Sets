223bc8c999c0915e4963bc63999514ed7b4f1b39
Zeehan0133 <zyne0133@gmail.com>
2021-11-26 01:20:04 +0530

    [zeehan] add:(remove item from set )

---
+++ 04_set.py
@@ -0,0 +1,16 @@
+"""
+@Author: Zeehan 
+@Date: 2021-25-11 19:00:30
+@Title : remove item from a set
+"""
+"""
+Description: declare variable city and remove item with the of remove method
+
+Parameter:
+      
+Return:
+"""
+city={"mumbai","nagpur","pune","bhopal"}
+print("Before remove :" ,(city))
+city.remove("bhopal")
+print("After remove :" ,(city))
