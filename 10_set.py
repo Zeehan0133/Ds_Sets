7324db58d07b4986213b4d17af0bda865187c1f1
Zeehan0133 <zyne0133@gmail.com>
2021-11-26 01:31:55 +0530

    [zeehan] add:(program to clear a set  )

---
+++ 10_set.py
@@ -0,0 +1,17 @@
+"""
+@Author: Zeehan 
+@Date: 2021-25-11 19:00:30
+@Title : program to clear a set.
+"""
+"""
+Description:
+          we can clear the sets with the help of clear method
+
+Parameter:
+      
+Return:
+"""
+seta={1,8,2,3}
+print("before clear :" , (seta))
+clr=seta.clear()
+print("after clear :" ,(clr))
